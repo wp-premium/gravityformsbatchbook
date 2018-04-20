f5c5f85da9ee24bbff806d842419772c8e28da7a
Gravity Forms <info@gravityforms.com>
2018-04-20 09:46:41 +0200

    1.3

--- batchbook.php
+++ batchbook.php
@@ -1,12 +1,18 @@
 <?php
-	
-/*
+
+// don't load directly
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+/**
 Plugin Name: Gravity Forms Batchbook Add-On
-Plugin URI: http://www.gravityforms.com
+Plugin URI: https://www.gravityforms.com
 Description: Integrates Gravity Forms with Batchbook, allowing form submissions to be automatically sent to your Batchbook account.
-Version: 1.2
+Version: 1.3
 Author: rocketgenius
-Author URI: http://www.rocketgenius.com
+Author URI: https://www.rocketgenius.com
+License: GPL-2.0+
 Text Domain: gravityformsbatchbook
 Domain Path: /languages
 
@@ -27,21 +33,47 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*/
+ **/
 
-define( 'GF_BATCHBOOK_VERSION', '1.2' );
+define( 'GF_BATCHBOOK_VERSION', '1.3' );
 
+// If Gravity Forms is loaded, bootstrap the Batchbook Add-On.
 add_action( 'gform_loaded', array( 'GF_Batchbook_Bootstrap', 'load' ), 5 );
 
+/**
+ * Class GF_Batchbook_Bootstrap
+ *
+ * Handles the loading of the Batchbook Add-On and registers with the Add-On Framework.
+ */
 class GF_Batchbook_Bootstrap {
 
+	/**
+	 * If the Feed Add-On Framework exists, Batchbook Add-On is loaded.
+	 *
+	 * @access public
+	 * @static
+	 */
 	public static function load() {
+
+		if ( ! method_exists( 'GFForms', 'include_feed_addon_framework' ) ) {
+			return;
+		}
+
 		require_once( 'class-gf-batchbook.php' );
+
 		GFAddOn::register( 'GFBatchbook' );
+
 	}
 
 }
 
+/**
+ * Returns an instance of the GFBatchbook class
+ *
+ * @see    GFBatchbook::get_instance()
+ *
+ * @return object GFBatchbook
+ */
 function gf_batchbook() {
 	return GFBatchbook::get_instance();
 }
